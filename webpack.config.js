1fb62db92845b20a2a00f05edfc70bb77ab83099
Shingo Konnai <k1ckap0l03@gmail.com>
2018-08-12 12:45:11 -0700

    finish setup

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  mode: 'production',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: 'public',
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.webpack.js', ' ', '.js', '.jsx']
+  }
+};
